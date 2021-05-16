6e9038faa0dba7fd5491cc4d2f9db3ac63aa8aca
Alejandro <mufinalamo@gmail.com>
2021-05-17 00:53:12 +0200

    Add files via upload

---
+++ EJERCICIOS/app2.js
@@ -0,0 +1,10 @@
+var letras = ['T', 'R', 'W', 'A', 'G', 'M', 'Y', 'F', 'P', 'D', 'X', 'B', 'N', 'J', 'Z', 'S', 'Q', 'V', 'H', 'L', 'C', 'K', 'E', 'T'];
+var NumeroDNI = 2;
+var LetraDNI = 'M'
+
+if (NumeroDNI <= 2){
+    return console.log('Este numero no es valido');
+}
+
+
+
